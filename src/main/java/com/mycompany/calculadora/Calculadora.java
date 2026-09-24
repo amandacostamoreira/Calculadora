b733e1c5d199c0d6ca8a4655dc5f9b8dc59d91ca
amandacostamoreira <amandamoreiracosta012@gmail.com>
2026-09-24 00:15:00 -0300

    capivara

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Cefet
+ */
+public class Calculadora {
+
+    // Cada método recebe dois double (números reais) e retorna double
+
+    public double somar(double a, double b) {
+        return a + b;
+    }
+
+    public double subtrair(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        // Proteção extra: evita erro de divisão por zero
+        if (b == 0) {
+            System.out.println("Erro: não é possível dividir por zero!");
+            return 0;
+        }
+        return a / b;
+    }
+}
+
